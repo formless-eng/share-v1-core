d7e0571bfebcc3aab94b0ace47b850a8e5e7ce1d
un7c0rn <brandon@formless.xyz>
2023-08-30 11:22:27 -0400

    Add specific instructions for SL2RD truffle migrations

--- migrations/2_SL2RD_migration.js
+++ migrations/2_SL2RD_migration.js
@@ -1,15 +1,6 @@
-const SHARE = artifacts.require("SHARE");
-const PFAUnit = artifacts.require("PFAUnit");
-const PFACollection = artifacts.require("PFACollection");
-const S2RD = artifacts.require("S2RD");
 const SL2RD = artifacts.require("SL2RD");
-const OperatorRegistry = artifacts.require("OperatorRegistry");
 const CodeVerification = artifacts.require("CodeVerification");
 const Immutable = artifacts.require("Immutable");
-const MockImmutable = artifacts.require("MockImmutable");
-const MockPFARevertsOnAccess = artifacts.require(
-  "MockPFARevertsOnAccess"
-);
 
 module.exports = async (deployer) => {
     // Deploy library contracts
