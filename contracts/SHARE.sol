7ed558cebc616931e15225dbebb88ddaf2493ca1
un7c0rn <brandon@formless.xyz>
2024-10-18 17:26:04 -0400

    Update dependency path

--- contracts/SHARE.sol
+++ contracts/SHARE.sol
@@ -12,7 +12,7 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/utils/introspection/ERC165.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "./libraries/CodeVerification.sol";
 import "./interfaces/IPFA.sol";
 
