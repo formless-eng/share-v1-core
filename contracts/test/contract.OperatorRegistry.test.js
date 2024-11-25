e09f41d33f4a4667a5526b8d6b5f0dbb88fb9b1a
un7c0rn <brandon@formless.xyz>
2024-11-25 06:54:57 -0500

    Check PR issues

--- contracts/test/contract.OperatorRegistry.test.js
+++ contracts/test/contract.OperatorRegistry.test.js
@@ -21,55 +21,43 @@ contract("OperatorRegistry", (accounts) => {
     );
   });
 
-  specify(
-    "Add new verified operator address to the registry",
-    async () => {
-      const operatorRegistry = await OperatorRegistry.deployed();
-      const newShareVerifiedOperator = accounts[5];
+  specify("Add new verified operator address to the registry", async () => {
+    const operatorRegistry = await OperatorRegistry.deployed();
+    const newShareVerifiedOperator = accounts[5];
 
-      await operatorRegistry.initialize(
-        verifiedShareOperatorEOAs /* shareEOAOperators_ */
-      );
-      operatorRegistry.addVerifiedOperator(newShareVerifiedOperator);
+    await operatorRegistry.initialize(
+      verifiedShareOperatorEOAs /* shareEOAOperators_ */
+    );
+    operatorRegistry.addVerifiedOperator(newShareVerifiedOperator);
 
-      assert(
-        operatorRegistry.isOperator(newShareVerifiedOperator),
-        "New SHARE operator address not properly added to registry."
-      );
-    }
-  );
+    assert(
+      operatorRegistry.isOperator(newShareVerifiedOperator),
+      "New SHARE operator address not properly added to registry."
+    );
+  });
 
-  specify(
-    "Remove a verified operator address from the registry",
-    async () => {
-      const operatorRegistry = await OperatorRegistry.deployed();
-      const addressToRemove = accounts[5];
-      await operatorRegistry.initialize(
-        verifiedShareOperatorEOAs /* shareEOAOperators_ */
-      );
+  specify("Remove a verified operator address from the registry", async () => {
+    const operatorRegistry = await OperatorRegistry.deployed();
+    const addressToRemove = accounts[5];
+    await operatorRegistry.initialize(
+      verifiedShareOperatorEOAs /* shareEOAOperators_ */
+    );
 
-      assert(
-        operatorRegistry.isOperator(addressToRemove),
-        `Operator address ${addressToRemove} does not exist in registry`
-      );
+    assert(
+      operatorRegistry.isOperator(addressToRemove),
+      `Operator address ${addressToRemove} does not exist in registry`
+    );
 
-      const success =
-        operatorRegistry.removeVerifiedOperator(addressToRemove);
+    const success = operatorRegistry.removeVerifiedOperator(addressToRemove);
 
-      assert(
-        success,
-        "Retired SHARE operator not removed from registry."
-      );
-    }
-  );
+    assert(success, "Retired SHARE operator not removed from registry.");
+  });
 
   specify("Fund the operator addresses", async () => {
     const operatorRegistry = await OperatorRegistry.deployed();
     const weiDeltaGranularity = 1000000;
     const totalFundingAmount = web3.utils.toWei("10", "ether");
-    const initialBalance = new Array(
-      verifiedShareOperatorEOAs.length
-    );
+    const initialBalance = new Array(verifiedShareOperatorEOAs.length);
 
     await operatorRegistry.initialize(
       verifiedShareOperatorEOAs /* shareEOAOperators_ */
@@ -113,7 +101,9 @@ contract("OperatorRegistry", (accounts) => {
         verifiedShareOperatorEOAs[i]
       );
 
-      const fundsDelta = newBalance - initialBalance[i];
+      const fundsDelta = web3.utils
+        .toBN(newBalance)
+        .sub(web3.utils.toBN(initialBalance[i]));
 
       console.log(
         `\nNew balance: ${newBalance}\nInitial balance: ${initialBalance[i]}\nFunds delta: ${fundsDelta}`
@@ -136,8 +126,7 @@ contract("OperatorRegistry", (accounts) => {
       verifiedShareOperatorEOAs /* shareEOAOperators_ */
     );
 
-    const registeredOperators =
-      await operatorRegistry.listOperatorAddresses();
+    const registeredOperators = await operatorRegistry.listOperatorAddresses();
 
     // The registeredOperators array should have the same length
     // as the verifiedShareOperatorEOAs array.
