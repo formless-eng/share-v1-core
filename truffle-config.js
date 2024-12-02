c69acce4d3af8251c09f9b21c6f83b03f6d3ea5b
xiangformless <xiang@formless.xyz>
2024-12-02 16:37:40 -0500

    fix balance

--- truffle-config.js
+++ truffle-config.js
@@ -16,7 +16,7 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*",
-      gas: 800000000,
+      gas: 80000000000000000,
     },
     ethereum: {
       provider: () =>
