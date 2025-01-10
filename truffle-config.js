3056a06d7c65faf169452114c94c166954e2bfed
un7c0rn <brandon@formless.xyz>
2025-01-10 10:52:11 -0500

    Add Base

--- truffle-config.js
+++ truffle-config.js
@@ -6,6 +6,7 @@ const RPC_ENDPOINT_OPTIMISM_SEPOLIA = process.env.RPC_ENDPOINT_OPTIMISM_SEPOLIA;
 const RPC_ENDPOINT_POLYGON_MUMBAI = process.env.RPC_ENDPOINT_POLYGON_MUMBAI;
 const RPC_ENDPOINT_POLYGON_MAINNET = process.env.RPC_ENDPOINT_POLYGON_MAINNET;
 const RPC_ENDPOINT_ETHEREUM_MAINNET = process.env.RPC_ENDPOINT_ETHEREUM_MAINNET;
+const RPC_ENDPOINT_BASE_MAINNET = process.env.RPC_ENDPOINT_BASE_MAINNET;
 const GAS_VALUE = process.env.GAS_VALUE ? process.env.GAS_VALUE : 5500000;
 
 const SOLIDITY_COMPILER_VERSION = "0.8.11";
@@ -77,6 +78,28 @@ module.exports = {
       provider: () =>
         new HDWalletProvider(mnemonicPhrase, RPC_ENDPOINT_OPTIMISM_MAINNET),
       network_id: 10,
+      gas: GAS_VALUE,
+      confirmations: 2,
+      timeoutBlocks: 2000,
+      networkCheckTimeout: 1000000,
+      skipDryRun: true,
+    },
+
+    optimism: {
+      provider: () =>
+        new HDWalletProvider(mnemonicPhrase, RPC_ENDPOINT_OPTIMISM_MAINNET),
+      network_id: 10,
+      gas: GAS_VALUE,
+      confirmations: 2,
+      timeoutBlocks: 2000,
+      networkCheckTimeout: 1000000,
+      skipDryRun: true,
+    },
+
+    base: {
+      provider: () =>
+        new HDWalletProvider(mnemonicPhrase, RPC_ENDPOINT_BASE_MAINNET),
+      network_id: 8453,
       gas: GAS_VALUE,
       confirmations: 2,
       timeoutBlocks: 2000,
