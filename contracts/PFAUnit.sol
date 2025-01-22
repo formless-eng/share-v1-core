7fa32f82cd547f0ae71c947913c1956a43e43ed2
un7c0rn <brandon@formless.xyz>
2025-01-22 00:02:51 -0500

    Initial implementation of ERC20 based approve call transfer pattern

--- contracts/PFAUnit.sol
+++ contracts/PFAUnit.sol
@@ -12,6 +12,7 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import "./PFA.sol";
 import "./libraries/CodeVerification.sol";
@@ -30,10 +31,7 @@ contract PFAUnit is IERC20Payable, ERC721, PFA {
     string public constant SYMBOL = "PFA";
     uint256 private constant UNIT_TOKEN_INDEX = 0;
     string internal _tokenURI;
-
-    // ERC20 contract address e.g. USDC
     address private _erc20ContractAddress;
-
     constructor()
         ERC721(NAME, SYMBOL)
         LimitedOwnable(true /* WALLET */, true /* SPLIT */)
@@ -66,11 +64,10 @@ contract PFAUnit is IERC20Payable, ERC721, PFA {
     /// of this contract, records a grant timestamp on chain which is
     /// read by decentralized distribution network (DDN) microservices
     /// to decrypt and serve the associated content for the tokenURI.
-    function access(
+    function accessNative(
         uint256 tokenId_,
         address recipient_
-    ) public payable override nonReentrant afterInit {
-        require(msg.value >= _pricePerAccess.value, "SHARE005");
+    ) internal payable nonReentrant afterInit {
         address owner = owner();
         // Since this contract is a LimitedOwnable, the code which
         // may reside at the owner address is restricted to approved
@@ -84,6 +81,79 @@ contract PFAUnit is IERC20Payable, ERC721, PFA {
         emit PaymentToOwner(owner, msg.value);
         emit Grant(recipient_, tokenId_);
         _transactionCount++;
+    }
+
+    function accessERC20(
+        uint256 tokenId_,
+        address recipient_
+    ) internal nonReentrant afterInit {
+        // 1. This function requires that the caller has approved
+        // a deduction within the USDC contract with this
+        // contract address as the spending entity.
+        SHARE protocol = SHARE(shareContractAddress());
+        address payeeAddress = owner();
+        // 2. If this contract is responsible for paying a wallet,
+        // this contract transfers the ERC20 token amount directly
+        // to the wallet.
+        if (
+            protocol.isApprovedBuild(
+                payeeAddress,
+                CodeVerification.BuildType.WALLET
+            )
+        ) {
+            IERC20(_erc20ContractAddress).transfer(
+                payeeAddress,
+                _pricePerAccess.value
+            );
+            _grantTimestamps[recipient_] = block.timestamp;
+            emit PaymentToOwner(payeeAddress, _pricePerAccess.value);
+            emit Grant(recipient_, tokenId_);
+            _transactionCount++;
+        } else if (
+            // A PFA contract may only push revenue
+            // to a wallet or a SHARE protocol split contract.
+            // Therefore, the only other valid build type is
+            // SPLIT.
+            // https://docs.google.com/document/d/1tbpUkDHqrQXVrEkiNVSPKvBtbHdclOMlNl3O5HUwj0M/edit?tab=t.0#bookmark=id.f75vop7llte
+            protocol.isApprovedBuild(
+                payeeAddress,
+                CodeVerification.BuildType.SPLIT
+            )
+        ) {
+            // 3. If this contract is responsible for paying a contract,
+            // this contract transfers the ERC20 token amount to itself,
+            // within the USDC contract, then executes approve and
+            // call on the downstream contract.
+            {
+                // Transfer
+                IERC20(_erc20ContractAddress).transfer(
+                    address(this),
+                    _pricePerAccess.value
+                );
+                // Approve
+                IERC20(_erc20ContractAddress).approve(
+                    payeeAddress,
+                    _pricePerAccess.value
+                );
+                // Call
+                (bool success, ) = payable(payeeAddress).call{value: 0}("");
+                require(success, "SHARE044");
+            }
+        } else {
+            revert("SHARE021");
+        }
+    }
+
+    function access(
+        uint256 tokenId_,
+        address recipient_
+    ) public payable override nonReentrant afterInit {
+        if (_erc20ContractAddress == address(0)) {
+            require(msg.value >= _pricePerAccess.value, "SHARE005");
+            accessNative(tokenId_, recipient_);
+        } else {
+            accessERC20(tokenId_, recipient_);
+        }
     }
 
     /// @notice Returns the token URI (ERC-721) for the asset.
@@ -108,8 +178,13 @@ contract PFAUnit is IERC20Payable, ERC721, PFA {
     }
 
     /// @notice Sets the ERC20 contract address (e.g., for USDC payments).
-    function setERC20ContractAddress(address contractAddress_) external override onlyOwner {
-        require(contractAddress_ != address(0), "Invalid ERC20 contract address");
+    function setERC20ContractAddress(
+        address contractAddress_
+    ) external override onlyOwner {
+        require(
+            contractAddress_ != address(0),
+            "Invalid ERC20 contract address"
+        );
         _erc20ContractAddress = contractAddress_;
     }
 
@@ -124,7 +199,7 @@ contract PFAUnit is IERC20Payable, ERC721, PFA {
         return
             interfaceId == type(IERC165).interfaceId ||
             interfaceId == type(IERC721).interfaceId ||
-            interfaceId == type(IPFA).interfaceId    ||
+            interfaceId == type(IPFA).interfaceId ||
             interfaceId == type(IERC20Payable).interfaceId;
     }
 }
