7fc6e6c76ae8887edd914220ffda4dca37cd4a36
un7c0rn <brandon@formless.xyz>
2025-01-26 14:25:44 -0500

    Improve comments

--- contracts/ERC20Payable.sol
+++ contracts/ERC20Payable.sol
@@ -14,18 +14,25 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/IERC20Payable.sol";
 
-/// @title Contract which implements it can receive payment using ERC20 tokens including USDC
+/// @title ERC20Payable - Contract for handling ERC20 token payments
 /// @author xiang@formless.xyz
+/// @notice Abstract contract that enables receiving payments in ERC20 tokens (e.g. USDC)
+/// @dev Inherits from IERC20Payable interface
 abstract contract ERC20Payable is IERC20Payable {
     address internal _erc20ContractAddress;
     IERC20 internal _erc20Token;
     uint256 internal constant ERC20_PAYABLE_CALL_VALUE = 0;
 
-    /// @notice Gets the ERC20 contract address used for payments.
+    /// @notice Retrieves the ERC20 token contract address used for payments
+    /// @return address The contract address of the ERC20 token
     function getERC20ContractAddress() external view returns (address) {
         return _erc20ContractAddress;
     }
 
+    /// @notice Sets the ERC20 token contract address for payments
+    /// @param contractAddress_ The address of the ERC20 token contract
+    /// @dev Initializes both the contract address and the IERC20 interface
+    /// @dev Reverts if a zero address is provided
     function _setERC20ContractAddress(address contractAddress_) internal {
         require(
             contractAddress_ != address(0),
@@ -35,10 +42,22 @@ abstract contract ERC20Payable is IERC20Payable {
         _erc20Token = IERC20(contractAddress_);
     }
 
+    /// @notice Checks if the contract is configured to accept ERC20 payments
+    /// @return bool True if ERC20 payments are enabled (contract address is set)
     function isERC20Payable() public view returns (bool) {
         return _erc20ContractAddress != address(0);
     }
 
+    /// @notice Transfers tokens from owner to spender and approves subsequent spending
+    /// @param tokenOwner_ Address of the token owner
+    /// @param tokenSpender_ Address that will spend the tokens
+    /// @param totalTokenAmount_ Total amount of tokens to transfer
+    /// @param callableContractAddress_ Contract address that will be approved to spend tokens
+    /// @param callableTokenAmount_ Amount of tokens to approve for spending
+    /// @dev Error codes:
+    ///      SHARE050: Insufficient allowance
+    ///      SHARE048: Transfer failed
+    ///      SHARE049: Approval failed
     function _transferERC20ThenApprove(
         address tokenOwner_,
         address tokenSpender_,
@@ -65,6 +84,12 @@ abstract contract ERC20Payable is IERC20Payable {
         );
     }
 
+    /// @notice Transfers tokens from message sender to a recipient
+    /// @param tokenRecipient_ Address that will receive the tokens
+    /// @param tokenAmount_ Amount of tokens to transfer
+    /// @dev Error codes:
+    ///      SHARE050: Insufficient allowance
+    ///      SHARE048: Transfer failed
     function _transferERC20FromSender(
         address tokenRecipient_,
         uint256 tokenAmount_
